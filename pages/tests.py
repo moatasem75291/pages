d398dde47daab08a8169d93907e9f5bf493f6bc4
mo3tsm <mo3tsm75291@gmail.com>
2022-08-16 20:40:17 +0200

    initial commit

---
+++ pages/tests.py
@@ -0,0 +1,40 @@
+from lib2to3.pgen2.literals import simple_escapes
+from urllib import response
+from django.test import TestCase, SimpleTestCase
+from django.urls import reverse
+
+
+class HomepageTests(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        respose = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def tes_template_content(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "<h1>Home page</h1>")
+
+
+class aboutbageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("/about/"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("about"))
+        self.assertTemplateUsed(response, "about.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("about"))
+        self.assertContains(response, "<h2>About Page</h2>")
